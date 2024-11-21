d042fe2275938dc2dfdbb75cb1110c921b484b0c
Ahmed Hassan <ahmedkiller0234@gmail.com>
2024-11-22 00:22:14 +0500

    iamgonnadie

--- src/index.js
+++ src/index.js
@@ -12,23 +12,20 @@ dotenv.config();
 const PORT = process.env.PORT;
 // Set up CORS to allow all origins
 
+app.use(
+  cors({
+    origin: "https://ahmedchatapp.vercel.app", // Allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Allow all HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"], // Allow specific headers
+    credentials: true, // If you're using cookies or authentication headers
+  })
+);
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://ahmedchatapp.vercel.app"); // Specify frontend URL
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
-
-app.use(cors()); // Default CORS
-
-app.use(
-  cors({
-    origin: "https://ahmedchatapp.vercel.app/", // Allow all origins
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Allow all HTTP methods
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"], // Allow specific headers
-    credentials: true, // If you're using cookies or authentication headers
-  })
-);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
